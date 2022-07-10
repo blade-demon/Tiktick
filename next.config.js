b7fa76c096beb745e7259fe65231569ba61c5109
blade-demon <xuziwei89@gmail.com>
2022-07-10 23:10:14 +0800

    complete main video config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["hips.hearstapps.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
