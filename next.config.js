3a7f8be345f9f38a585ed3f0bde06035d8c83d57
blade-demon <xuziwei89@gmail.com>
2022-07-11 23:55:32 +0800

    complete config for google logim

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["hips.hearstapps.com"],
+    domains: ["hips.hearstapps.com", "lh3.googleusercontent.com"],
   },
 };
 
